acc2e75e4592ab0d327f2b4b009db5ee5f03bcd2
Wenqi Liao <wl2250@nyu.edu>
2022-11-04 15:15:26 -0400

    install bcryptjs

--- db.mjs
+++ db.mjs
@@ -17,7 +17,29 @@ const CommentSchema = new mongoose.Schema({
     createdAt: {type: Timestamp, required: false}
 });
 
-User.find({})
-    .populate('comments')
-    .exec()
+mongoose.model('User', UserSchema);
+mongoose.model('Comment', CommentSchema);
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/wl2250';
+}
+
+mongoose.connect(dbconf);
 
