332160a45a1f25ea5e981e53e43913e8b8f5eb63
Wenqi Liao <wl2250@nyu.edu>
2022-11-28 23:27:38 -0500

    create mocha unit test for the user database

--- test/db_test.mjs
+++ test/db_test.mjs
@@ -1,16 +1,10 @@
 import '../db.mjs';
 import * as mongoose from 'mongoose';
-import {Mockgoose} from 'mockgoose';
-let mockgoose = new Mockgoose(mongoose);
+import chai from 'chai';
+import assert from 'assert';
+let should = chai.should();
 const User = mongoose.model('User');
-
-before(function(done) {
-	mockgoose.prepareStorage().then(function() {
-		mongoose.connect('mongodb://localhost/wl2250', function(err) {
-			done(err);
-		});
-	});
-});
+mongoose.connect('mongodb://localhost/wl2250');
 
 describe('save user', function() {
 	it("fails to save without name", function(done) {
@@ -23,4 +17,61 @@ describe('save user', function() {
             done();
         });
 	});
+    it("fails to save without email(username)", function(done) {
+		const newUser = new User({
+            name: 'lwq',
+            relationship: 'self'
+        });
+        newUser.save(function(err) {
+            should.exist(err);
+            done();
+        });
+	});
+    it("fails to save without relationship", function(done) {
+		const newUser = new User({
+            username: '1051251498@qq.com',
+            name: 'lwq',
+        });
+        newUser.save(function(err) {
+            should.exist(err);
+            done();
+        });
+	});
 });
+
+describe('find user using relationship', function() {
+    it("will found user whose relationship is self", function(done) {
+		const newUser = new User({
+            username: 'test@qq.com',
+            relationship: 'self',
+            name: 'lwq'
+        });
+        newUser.save() 
+            .then(
+                User.findOne({ relationship: 'self' })
+                .then((user) => {
+                    assert(user.relationship === 'self'); 
+                    done();
+                })
+            )  
+	});
+});
+const newUser = new User({
+    username: 'test1@qq.com',
+    relationship: 'self',
+    name: 'lwq'
+});
+newUser.save();
+// source: https://www.geeksforgeeks.org/how-to-use-mocha-with-mongoose/
+describe('Delete a user', function() {
+    it('Removes a user', (done) => {
+        User.findOneAndRemove({ username: 'test1@qq.com' })
+            .then(() => User.findOne({ username: 'test1@qq.com' }))
+            .then((user) => {
+                assert(user == null);
+                done();
+            });
+        });
+});
+
+after(() => { mongoose.disconnect() });
